fc80f1cb0f850fe9706485824bd052e1a5320e6f
Ирина Зесли <zesli@mail.ru>
2020-04-29 21:10:07 +0300

    Определение расст. м/у 2-мя точками при помощи метода класса Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -8,4 +8,8 @@ public class Point {
     this.x = x;
     this.y = y;
   }
+
+  public double distance(Point this, Point other){
+    return Math.sqrt((this.x - other.x)*(this.x - other.x) + (this.y - other.y)*(this.y - other.y));
+  }
 }
