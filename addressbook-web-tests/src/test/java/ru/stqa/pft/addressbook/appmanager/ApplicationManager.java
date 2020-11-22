ed8d0dd4713500c89a7fc8c5c1d7189833fdeb4b
Ирина Зесли <zesli@mail.ru>
2020-11-22 20:30:42 +0300

    Увеличила ожидание

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -51,7 +51,7 @@ public class ApplicationManager {
       capabilities.setBrowserName(browser);
       wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")),capabilities);
     }
-    wd.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
+    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
